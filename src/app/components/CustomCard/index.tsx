5668753579fe27b4a2e1d4cfffa8110189a55401
tuorjp <jplmarotinho22@gmail.com>
2025-01-03 16:24:00 -0300

    motion lib added, card animation

--- src/app/components/CustomCard/index.tsx
+++ src/app/components/CustomCard/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material"
 import { useRouter } from "next/navigation"
+import * as motion from "motion/react-client"
 
 type CustomCardProps = {
   title?: string,
@@ -11,29 +12,35 @@ export function CustomCard({ text, route, title }: CustomCardProps) {
   const router = useRouter()
 
   return (
-    <Box
-      sx={{
-        boxShadow: 4,
-        borderRadius: 1.5,
-        display: 'flex',
-        flexDirection: 'column',
-        p: 2,
-        cursor: route ? 'pointer' : 'auto'
-      }}
-      onClick={() => {
-        if(route) {
-          router.push(route)
-        }
-      }}
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.8 }}
     >
-      <Typography variant="h6" mb={2} sx={{ borderBottom: '1px dashed', borderColor: '#94a3b8' }}>
-        {title}
-      </Typography>
-      <Box>
-        <Typography variant="body1">
-          {text}
+      <Box
+        sx={{
+          boxShadow: 4,
+          borderRadius: 1.5,
+          display: 'flex',
+          flexDirection: 'column',
+          p: 2,
+          cursor: route ? 'pointer' : 'auto',
+          background:'#fff'
+        }}
+        onClick={() => {
+          if (route) {
+            router.push(route)
+          }
+        }}
+      >
+        <Typography variant="h6" mb={2} sx={{ borderBottom: '1px dashed', borderColor: '#94a3b8' }}>
+          {title}
         </Typography>
+        <Box>
+          <Typography variant="body1">
+            {text}
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </motion.div>
   )
 }
